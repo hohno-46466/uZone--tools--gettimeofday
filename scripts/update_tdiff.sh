8e19dc01f40c5fe58b92e2320cc05c514321bf7d
hohno-46466 <hohno.46466@gmail.com>
2021-09-06 13:04:37 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

--- scripts/update_tdiff.sh
+++ scripts/update_tdiff.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 
-TDIFF_FILE=$HOME/etc/timediff
+# gettimeofday
+
+# See also: git@github.com:hohno-46466/uZone--tools--gettimeofday.git
+
 DEBUG=N
 
-x=-1
+TDIFF_FILE=$HOME/etc/timediff
+GUARD_TIME=60
+
+td=-1
 
 if [ "x$1" = "x-d" -o "x$1" = "x--debug" ]; then
   DEBUG=Y
@@ -13,21 +19,22 @@ if [ -f ${TDIFF_FILE} ]; then
   z=$(egrep '^[0-9]* [0-9]*' $TDIFF_FILE | head -1)
   if [ "x$z" != "x" ]; then
     set $z
-    z1=$1 # unix time
-    z2=$2 # offset in sec
-    t1=$(/bin/date +%s)
-    t0=$(( $t1 - $z1 ))
-    [ "x$DEBUG" = "xY" ] && echo "DEBUG: ($z) -> ($z1) ($z2) || ($t1) - ($z1) = ($t0)"
+    T1s=$1 # unix time in the TDIFF_FILE
+    T1u=$2 # offset in sec in the TDIFF_FILE
+    T0s=$(/bin/date +%s) # current time
+    td=$(( $T0s - $T1s ))
+    [ "x$DEBUG" = "xY" ] && echo "DEBUG: ($z) -> ($T1s) ($T1u) || ($T0s) - ($T1s) = ($td)"
   fi
 fi
 
-if [ $t0 -ge 0 -a $t0 -lt 30 ] ; then
-  [ "x$DEBUG" = "xY" ] && echo -n "(1) "
-  echo "$z1 $z2" | tee $TDIFF_FILE
+if [ $td -ge 0 -a $td -lt $GUARD_TIME ] ; then
+  [ "x$DEBUG" = "xY" ] && echo -n "DEBUG: (1) "
+  echo "$T1s $T1u" | tee $TDIFF_FILE
 else
   z2=$(/usr/sbin/ntpdate -q ntp.nict.jp | tail -1 | awk '{print $10}')
-  z1=$(/bin/date +%s) # z1 must be set after z2 is ready
-  [ "x$DEBUG" = "xY" ] && echo -n "(2) "
+  z1=$(/bin/date +%s) # z1 must be set after z2 has been set. Because settings z2 takes long time.
+  [ "x$DEBUG" = "xY" ] && echo -n "DEBUG: (2) "
   echo "$z1 $z2" | tee $TDIFF_FILE
 fi
 
+exit 0
