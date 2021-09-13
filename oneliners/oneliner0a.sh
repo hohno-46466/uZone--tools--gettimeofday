cf9d4e2405d8c78ea91d66a15aa03a4c5e72611f
hohno-46466 <hohno.46466@gmail.com>
2021-09-14 08:53:52 +0900

    updated by @hohno_at_kuimc on hohno-EEU-2020C

--- oneliners/oneliner0a.sh
+++ oneliners/oneliner0a.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
+# set -x
 echo "$(/usr/sbin/ntpdate -q ntp.nict.jp | tail -1 | awk '{print $10}') $(/bin/date +%s)" | awk '{print $2, $1}'
--- oneliners/oneliner0b.sh
+++ oneliners/oneliner0b.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
+# set -x
 echo "export gtdOffset=$(/usr/sbin/ntpdate -q ntp.nict.jp | tail -1 | awk '{print $10}')"
