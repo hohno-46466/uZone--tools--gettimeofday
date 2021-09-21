6bbd2d86b3ed3a8cc37aafe6b3273e5d8ee4890e
hohno-46466 <hohno.46466@gmail.com>
2021-09-21 20:38:50 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2016B.local

--- oneliners/oneliner0a.sh
+++ oneliners/oneliner0a.sh
@@ -2,5 +2,6 @@
 
 # set -x
 ntpdate=/usr/sbin/ntpdate
+# ntpdate=/usr/local/sbin/ntpdate
 # ntpdate=/opt/homebrew/sbin/ntpdate
 echo "$($ntpdate -q ntp.nict.jp | tail -1 | awk '{print $10}') $(/bin/date +%s)" | awk '{print $2, $1}'
--- oneliners/oneliner0b.sh
+++ oneliners/oneliner0b.sh
@@ -2,5 +2,6 @@
 
 # set -x
 ntpdate=/usr/sbin/ntpdate
+# ntpdate=/usr/local/sbin/ntpdate
 # ntpdate=/opt/homebrew/sbin/ntpdate
 echo "export gtdOffset=$($ntpdate -q ntp.nict.jp | tail -1 | awk '{print $10}')"
